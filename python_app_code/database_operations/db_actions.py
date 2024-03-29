a7490d7dc6d490c7fc0bb3a8d9f779419121b74e
L. Martin <41234752+martin059@users.noreply.github.com>
2024-03-29 18:30:28 +0100

    #24 add put due by operation (#35)
    * Renamed function and added new one
    
    * Refactored function to reduce its size
    
    * Refactored insert/update due by function
    
    * Added function to see if specific due_date for a given task is already present
    
    * Refactored class, deleted duplicated code and enforce better REST requests standards
    
    * Refactored class, added new put due-date method
    
    * Fixed validation to take into account negative integers
    
    * Fixed if logic

--- python_app_code/database_operations/db_actions.py
+++ python_app_code/database_operations/db_actions.py
@@ -69,6 +69,22 @@ def get_due_by(task_id: str):
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
         return None
+    
+def get_specific_due_by(task_id: str, due_date: str):
+    """ Retrieve a specific due date of a given task from the Due_by table """
+
+    select_query_base = 'SELECT * FROM app."Due_by" WHERE task_id = {0} AND due_date = {1}'
+    select_query = sql.SQL(select_query_base).format(sql.Literal(task_id), sql.Literal(due_date))
+    config  = dbc.load_config()
+    try:
+        with psycopg2.connect(**config) as conn:
+            with conn.cursor(cursor_factory=RealDictCursor) as cur:
+                cur.execute(select_query)
+                return cur.fetchall()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return error
 
 def compose_task_insert_update_values(task_name, task_descrip, creation_date, task_status):
     column_names = []
@@ -214,15 +230,7 @@ def insert_into_due_by_table(task_id, due_date):
     Returns:
     - str: A message indicating the result of the operation.
     """
-    # Prepare SQL queries and connection settings
-    select_query_base = 'SELECT * FROM app."Due_by" WHERE task_id = {0}'
-    select_query = sql.SQL(select_query_base).format(sql.Literal(task_id))
-    update_deactivate_query_base = 'UPDATE app."Due_by" SET is_active = false WHERE task_id = {0} AND is_active = true'
-    update_activate_query_base = 'UPDATE app."Due_by" SET is_active = true WHERE task_id = {0} AND due_date = {1}'
-    update_deactivate_query = sql.SQL(update_deactivate_query_base).format(sql.Literal(task_id))
-    update_activate_query = sql.SQL(update_activate_query_base).format(sql.Literal(task_id), sql.Literal(due_date))
-    insert_query_base = 'INSERT INTO app."Due_by" (task_id, due_date, is_active) VALUES ({0}, {1}, true)'
-    insert_query = sql.SQL(insert_query_base).format(sql.Literal(task_id), sql.Literal(due_date))
+    select_query, update_deactivate_query, update_activate_query, insert_query = prep_insert_due_date_queries(task_id, due_date)
     config = dbc.load_config()
     msg = None
     try:
@@ -231,29 +239,17 @@ def insert_into_due_by_table(task_id, due_date):
                 # Check if the task has an active due date
                 cur.execute(select_query)
                 rows = cur.fetchall()
-                update_necessary = True
+                modification_necessary = True
                 due_date_already_present = False
                 if rows:
-                    active_due_date = None
-                    for row in rows:
-                        if row["is_active"]:
-                            active_due_date = row["due_date"]
-                        if str(row["due_date"]) == due_date:
-                            due_date_already_present = True
-                    # If the new due date is different from the active one, update the active row
-                    if str(active_due_date) != due_date:
-                        cur.execute(update_deactivate_query) # Deactivate previous old due date
-                        msg = "Updated active due date"
-                    else:
-                        update_necessary = False
-                        msg = "New due date is the same as the active one"
-                # If update is necessary, and due date is already present, activate row
-                if update_necessary:
+                    modification_necessary, due_date_already_present, msg = check_current_due_date(cur, rows, due_date, update_deactivate_query)
+                # If modification is necessary, and due date is already present, activate row
+                if modification_necessary:
                     if due_date_already_present:
                         cur.execute(update_activate_query)
                         msg = f"Updated active due date for task id: {task_id}"
-                    # If update is necessary, and new due date is not present, insert a new row
                     else:
+                        # If modification is necessary, and new due date is not present, insert a new row
                         cur.execute(insert_query)
                         msg = f"Insert new active due date for task id: {task_id}"
                     conn.commit()
@@ -261,6 +257,36 @@ def insert_into_due_by_table(task_id, due_date):
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
         return error
+    
+def prep_insert_due_date_queries(task_id, due_date):
+    """ Prepare SQL queries for inserting/updating due dates """
+    select_query_base = 'SELECT * FROM app."Due_by" WHERE task_id = {0}'
+    select_query = sql.SQL(select_query_base).format(sql.Literal(task_id))
+    update_deactivate_query_base = 'UPDATE app."Due_by" SET is_active = false WHERE task_id = {0} AND is_active = true'
+    update_activate_query_base = 'UPDATE app."Due_by" SET is_active = true WHERE task_id = {0} AND due_date = {1}'
+    update_deactivate_query = sql.SQL(update_deactivate_query_base).format(sql.Literal(task_id))
+    update_activate_query = sql.SQL(update_activate_query_base).format(sql.Literal(task_id), sql.Literal(due_date))
+    insert_query_base = 'INSERT INTO app."Due_by" (task_id, due_date, is_active) VALUES ({0}, {1}, true)'
+    insert_query = sql.SQL(insert_query_base).format(sql.Literal(task_id), sql.Literal(due_date))
+    return select_query, update_deactivate_query, update_activate_query, insert_query
+
+def check_current_due_date(cur, due_date_rows, due_date, update_deactivate_query):
+    modification_necessary = True
+    due_date_already_present = False
+    active_due_date = None
+    for row in due_date_rows:
+        if row["is_active"]:
+            active_due_date = row["due_date"]
+        if str(row["due_date"]) == due_date:
+            due_date_already_present = True
+    # If the new due date is different from the active one, update the active row
+    if str(active_due_date) != due_date:
+        cur.execute(update_deactivate_query) # Deactivate previous old due date
+        msg = "Updated active due date"
+    else:
+        modification_necessary = False
+        msg = "New due date is the same as the active one"
+    return modification_necessary, due_date_already_present, msg
 
 # The following two operations are for debugging since they print directly their results to the console
 def print_all_tasks():
--- python_app_code/rest_api/tasks_operations_api.py
+++ python_app_code/rest_api/tasks_operations_api.py
@@ -11,43 +11,49 @@ def list_tasks_route(task_id = None):
       if task_id is None:
          return list_tasks()
       else:
-         return get_task(task_id)
+         if is_task_id_valid(task_id):
+            return get_task(task_id)
+         else:
+            return jsonify({'error': 'Parameter "task-id" in "/tasks/<task-id>" must be a valid integer.'}), 400
    elif request.method == "POST":
       return add_task(request)
    elif request.method == "DELETE":
       if task_id is None:
          return jsonify({'error': 'Parameter "task-id" in "/tasks/<task-id>/" must be given to delete a task.'}), 400
       else:
-         return delete_task(task_id)
+         if is_task_id_valid(task_id):
+            return delete_task(task_id)
+         else:
+            return jsonify({'error': 'Parameter "task-id" in "/tasks/<task-id>/" must be a valid integer.'}), 400
    elif request.method == "PUT":
       if task_id is None:
          return jsonify({'error': 'Parameter "task-id" in "/tasks/<task-id>/" must be given to update a task.'}), 400
       else:
-         return update_task(task_id, request)
+         if is_task_id_valid(task_id):
+            return update_task(task_id, request)
+         else:
+            return jsonify({'error': 'Parameter "task-id" in "/tasks/<task-id>/" must be a valid integer.'}), 400
    
-@tasks_api.route('/tasks/<task_id>/due-by', methods=['GET', 'POST'])
+@tasks_api.route('/tasks/<task_id>/due-by', methods=['GET', 'POST', 'PUT'])
 def list_due_by_route(task_id):
+   if not is_task_id_valid(task_id):
+         return jsonify({'error': 'Parameter "task-id" in "/tasks/<task-id>/due-by" must be a valid integer.'}), 400
    if request.method == 'GET':
-      if task_id is None:
-         return jsonify({'error': 'Parameter "task-id" in "/tasks/<task-id>/due-by" must be given to get a task\'s due date.'}), 400 
-      else:
-         return get_task_due_dates(task_id)
+      return get_task_due_dates(task_id)
    elif request.method == "POST":
       return post_due_date(task_id, request)
+   elif request.method == "PUT":
+      return put_due_date(task_id, request)
    
 def list_tasks():
    return dba.get_all_tasks()
 
 def get_task(id: str):
-   try:
-      id = int(id)
-      db_response = dba.get_a_task(id)
-      if db_response is None or len(db_response) == 0:
-         return jsonify({'error': 'Task with id {} not found'.format(id)}), 404
-      else:
-         return db_response
-   except ValueError:
-      return jsonify({'error': 'Parameter "id" in "/tasks/<id>" must be an integer.'}), 400
+   db_response = dba.get_a_task(id)
+   if db_response is None or len(db_response) == 0:
+      return jsonify({'error': 'Task with id {} not found'.format(id)}), 404
+   else:
+      return db_response
    
 def get_task_due_dates(id: str):
    try:
@@ -65,7 +71,7 @@ def add_task(received_request: request):
    #The code won't validate if the creation_date or the due_date have a valid format
    tmp = validate_json(received_request)
    if tmp[1] == 200:
-      new_task = parse_new_task_request(received_request.get_json())
+      new_task = parse_received_request(received_request.get_json())
       new_task_id = dba.insert_into_task_table(new_task["task_name"], new_task["task_descrip"], 
                                  new_task["creation_date"], new_task["task_status"], new_task["due_date"])
       return jsonify({"new_task_id": new_task_id}), 201
@@ -73,7 +79,6 @@ def add_task(received_request: request):
       return tmp[0], tmp[1]
 
 def post_due_date(task_id, received_request: request):
-   #The code won't validate if the due_date has a valid format
    id = int(task_id)
    task = dba.get_a_task(id)
    if task is None or len(task) == 0:
@@ -81,9 +86,31 @@ def post_due_date(task_id, received_request: request):
    else:
       tmp = validate_due_date_json(received_request)
       if tmp[1] == 200:
-         new_due_date = parse_new_task_request(received_request.get_json())
-         response = dba.insert_into_due_by_table(id, new_due_date["due_date"])
-
+         parsed_req = parse_received_request(received_request.get_json())
+         check_if_due_date_exists = dba.get_specific_due_by(id, parsed_req["due_date"])
+         if check_if_due_date_exists is not None and len(check_if_due_date_exists) > 0:
+            return jsonify({"error": "Due date already exists for this task, use PUT method instead to update it."}), 409
+         response = dba.insert_into_due_by_table(id, parsed_req["due_date"])
+         if not(isinstance(response, Exception)):
+            return jsonify({"message": response}), 201
+         else:
+            return jsonify({"error": str(response)}), 400
+      else:
+         return tmp[0], tmp[1]
+      
+def put_due_date(task_id, received_request: request):
+   id = int(task_id)
+   task = dba.get_a_task(id)
+   if task is None or len(task) == 0:
+      return jsonify({'error': 'Task with id {} not found, create a task beforehand'.format(id)}), 404
+   else:
+      tmp = validate_due_date_json(received_request)
+      if tmp[1] == 200:
+         parsed_req = parse_received_request(received_request.get_json())
+         check_if_due_date_exists = dba.get_specific_due_by(id, parsed_req["due_date"])
+         if check_if_due_date_exists is None or len(check_if_due_date_exists) == 0:
+            return jsonify({"error": "Due date doesn't exists for this task, use POST method instead to create it."}), 404
+         response = dba.insert_into_due_by_table(id, parsed_req["due_date"])
          if not(isinstance(response, Exception)):
             return jsonify({"message": response}), 200
          else:
@@ -99,7 +126,7 @@ def update_task(task_id, received_request: request):
    else:
       tmp = validate_json(received_request, False)
       if tmp[1] == 200:
-         update_task = parse_new_task_request(received_request.get_json())
+         update_task = parse_received_request(received_request.get_json())
          response = dba.update_task(id, update_task["task_name"], update_task["task_descrip"], 
                                     update_task["creation_date"], update_task["task_status"], update_task["due_date"])
          if response is None:
@@ -137,7 +164,17 @@ def validate_json(received_request: request, needs_to_have_name: bool = True):
 
 def has_task_name(new_task_to_validate: dict):
    # Returns True if the JSON data has a field named "task_name", False otherwise.
+   if new_task_to_validate is None:
+      return False
    return "task_name" in new_task_to_validate
+
+def is_task_id_valid(task_id: str):
+   # Returns True if the task_id is an integer, False otherwise.
+   if task_id is None:
+      return False
+   if not isinstance(task_id, (int, float, str, bool)):
+      return False
+   return re.match(r'^-?\d+$', task_id)
    
 def validate_due_date_json(received_request: request):
    if not received_request.json:  # Check if the request body is empty
@@ -146,20 +183,22 @@ def validate_due_date_json(received_request: request):
    if received_request.is_json:
        try:
            req = received_request.get_json()
-           if has_due_date(req):
+           if has_valid_due_date(req):
               return jsonify({"message": "Valid JSON received"}), 200
            else:
-              return jsonify({"error": "A new Due Date must have a 'due_date'."}), 400
+              return jsonify({"error": "A new Due Date must have a valid 'due_date' with the 'YYYY-MM-DD' format."}), 400
        except Exception as e:
            return jsonify({"error": "Invalid JSON format", "details": str(e)}), 400
    else:
        return jsonify({"error": "Request does not contain JSON data"}), 400
 
-def has_due_date(new_due_date_to_validate: dict):
-   # Returns True if the JSON data has a field named "due_date", False otherwise.
-   return "due_date" in new_due_date_to_validate
+def has_valid_due_date(new_due_date_to_validate: dict):
+   # Returns True if the JSON data has a field named "due_date" with a valid format, False otherwise.
+   return ("due_date" in new_due_date_to_validate and 
+           isinstance(new_due_date_to_validate["due_date"], str) and 
+           re.match(r'^\d{4}-\d{2}-\d{2}$', new_due_date_to_validate["due_date"]))
 
-def parse_new_task_request(received_request: dict):
+def parse_received_request(received_request: dict):
    parsed_req = {}
    parsed_req["task_name"] = received_request.get("task_name") if isinstance(received_request, dict) else None
    parsed_req["task_descrip"] = received_request.get("task_descrip") if isinstance(received_request, dict) else None
