b2b24c1e82b399f8cd15dd1fb772467bf4a413ec
martin059 <41234752+martin059@users.noreply.github.com>
2024-03-29 22:59:05 +0100

    Refactor common operations to new utils class

--- python_app_code/rest_api/tasks_operations_api.py
+++ python_app_code/rest_api/tasks_operations_api.py
@@ -1,6 +1,7 @@
 from flask import request, Blueprint, jsonify
 import re
 from database_operations import db_actions as dba
+import api_operations_utils as aou
 
 tasks_api = Blueprint('tasks_api', __name__)
 
@@ -17,7 +18,7 @@ def list_tasks_route(task_id: int = None) -> jsonify:
       JSON response: The response containing the result of the operation or an error message.
    """
    if task_id is not None:
-      if not is_task_id_valid(task_id):
+      if not aou.is_task_id_valid(task_id):
          return jsonify({'error': 'Parameter "task-id" in "/tasks/<task-id>" must be a valid integer.'}), 400
       
    if request.method == 'GET':
@@ -49,7 +50,7 @@ def list_due_by_route(task_id: int) -> jsonify:
    Returns:
       jsonify: The response containing the due dates or an error message.
    """
-   if not is_task_id_valid(task_id):
+   if not aou.is_task_id_valid(task_id):
       return jsonify({'error': 'Parameter "task-id" in "/tasks/<task-id>/due-by" must be a valid integer.'}), 400
    if request.method == 'GET':
       return get_task_due_dates(task_id)
@@ -124,11 +125,11 @@ def add_task(received_request: request) -> jsonify:
    Returns:
       A JSON response containing the new task ID if successful, or an error message if unsuccessful.
    """
-   tmp = validate_json(received_request)
+   tmp = aou.validate_json(received_request)
    if tmp[1] == 200:
-      new_task = parse_received_request(received_request.get_json())
+      new_task = aou.parse_received_request(received_request.get_json())
       if new_task["due_date"] is not None:
-         if not validate_due_date_json(received_request):
+         if not aou.validate_due_date_json(received_request):
             return jsonify({"error": "A Due Date must have a valid 'due_date' with the 'YYYY-MM-DD' format."}), 400
       try:
          response = dba.insert_into_task_table(new_task["task_name"], new_task["task_descrip"], 
@@ -163,9 +164,9 @@ def post_due_date(task_id: int, received_request: request, comming_from_put: boo
       if task is None or len(task) == 0:
          return jsonify({'error': 'Task with id {} not found, create a task beforehand'.format(id)}), 404
       else:
-         tmp = validate_due_date_json(received_request)
+         tmp = aou.validate_due_date_json(received_request)
          if tmp[1] == 200:
-            parsed_req = parse_received_request(received_request.get_json())
+            parsed_req = aou.parse_received_request(received_request.get_json())
             check_if_due_date_exists = dba.get_specific_due_by(id, parsed_req["due_date"])
             if not comming_from_put:
                if check_if_due_date_exists is not None and len(check_if_due_date_exists) > 0:
@@ -211,11 +212,11 @@ def update_task(task_id: int, received_request: request) -> jsonify:
       if task is None or len(task) == 0:
          return jsonify({'error': 'Task with id {} not found'.format(id)}), 404
       else:
-         tmp = validate_json(received_request, False)
+         tmp = aou.validate_json(received_request, False)
          if tmp[1] == 200:
-            update_task = parse_received_request(received_request.get_json())
+            update_task = aou.parse_received_request(received_request.get_json())
             if update_task["due_date"] is not None:
-               if not validate_due_date_json(received_request):
+               if not aou.validate_due_date_json(received_request):
                   return jsonify({"error": "A Due Date must have a valid 'due_date' with the 'YYYY-MM-DD' format."}), 400
             response = dba.update_task(id, update_task["task_name"], update_task["task_descrip"], 
                                  update_task["creation_date"], update_task["task_status"], update_task["due_date"])
@@ -246,119 +247,3 @@ def delete_task(task_id: int) -> jsonify:
      return '', 204
    except Exception as e:
      return jsonify({'error': str(e)}), 500
-      
-    
-def validate_json(received_request: request, needs_to_have_name: bool = True) -> jsonify:
-   """
-   Validates the JSON data in the received request.
-
-   Args:
-      received_request (request): The request object containing the JSON data.
-      needs_to_have_name (bool, optional): Indicates whether the JSON data needs to have a 'task_name' field. Defaults to True.
-
-   Returns:
-      jsonify: A JSON response indicating the validation result.
-   """
-   if not received_request.json:  # Check if the request body is empty
-      return jsonify({'error': 'Empty request body'}), 400
-
-   if received_request.is_json:
-      try:
-         req = received_request.get_json()
-         if not needs_to_have_name:
-            return jsonify({"message": "Valid JSON received"}), 200
-         elif has_task_name(req):
-            return jsonify({"message": "Valid JSON received"}), 200
-         else:
-            return jsonify({"error": "A new Task must have a 'task_name'."}), 400
-      except Exception as e:
-         return jsonify({"error": "Invalid JSON format", "details": str(e)}), 400
-   else:
-      return jsonify({"error": "Request does not contain JSON data"}), 400
-
-def has_task_name(new_task_to_validate: dict) -> bool:
-   """
-   Checks if the given JSON data contains a field named "task_name".
-
-   Args:
-      new_task_to_validate (dict): The JSON data to validate.
-
-   Returns:
-      bool: True if the JSON data has a field named "task_name", False otherwise.
-   """
-   if new_task_to_validate is None:
-      return False
-   return "task_name" in new_task_to_validate
-
-def is_task_id_valid(task_id: str) -> bool:
-   """
-   Check if the task_id is valid.
-
-   Args:
-      task_id (str): The task_id to be checked.
-
-   Returns:
-      bool: True if the task_id is an integer, False otherwise.
-   """
-   if task_id is None:
-      return False
-   if not isinstance(task_id, (int, float, str, bool)):
-      return False
-   return re.match(r'^-?\d+$', task_id)
-   
-def validate_due_date_json(received_request: request) -> jsonify:
-   """
-   Validates the JSON data in the request body to ensure it contains a valid due date.
-
-   Args:
-      received_request (request): The request object containing the JSON data.
-
-   Returns:
-      jsonify: A JSON response indicating the result of the validation.
-   """
-   if not received_request.json:  # Check if the request body is empty
-      return jsonify({'error': 'Empty request body'}), 400
-
-   if received_request.is_json:
-      try:
-         req = received_request.get_json()
-         if has_valid_due_date(req):
-            return jsonify({"message": "Valid JSON received"}), 200
-         else:
-            return jsonify({"error": "A new Due Date must have a valid 'due_date' with the 'YYYY-MM-DD' format."}), 400
-      except Exception as e:
-         return jsonify({"error": "Invalid JSON format", "details": str(e)}), 400
-   else:
-      return jsonify({"error": "Request does not contain JSON data"}), 400
-
-def has_valid_due_date(new_due_date_to_validate: dict) -> bool:
-   """
-   Checks if the JSON data has a field named "due_date" with a valid format.
-
-   Args:
-      new_due_date_to_validate (dict): The JSON data to validate.
-
-   Returns:
-      bool: True if the JSON data has a valid "due_date" field, False otherwise.
-   """
-   return ("due_date" in new_due_date_to_validate and 
-         isinstance(new_due_date_to_validate["due_date"], str) and 
-         re.match(r'^\d{4}-\d{2}-\d{2}$', new_due_date_to_validate["due_date"]))
-
-def parse_received_request(received_request: dict) -> dict:
-   """
-   Parses the received request and extracts relevant information.
-
-   Args:
-      received_request (dict): The received request as a dictionary.
-
-   Returns:
-      dict: A dictionary containing the parsed information from the received request.
-   """
-   parsed_req = {}
-   parsed_req["task_name"] = received_request.get("task_name") if isinstance(received_request, dict) else None
-   parsed_req["task_descrip"] = received_request.get("task_descrip") if isinstance(received_request, dict) else None
-   parsed_req["creation_date"] = received_request.get("creation_date") if isinstance(received_request, dict) else None
-   parsed_req["task_status"] = received_request.get("task_status") if isinstance(received_request, dict) else None
-   parsed_req["due_date"] = received_request.get("due_date") if isinstance(received_request, dict) else None
-   return parsed_req
