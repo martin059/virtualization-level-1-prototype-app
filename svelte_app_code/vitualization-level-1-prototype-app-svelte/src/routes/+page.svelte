214b0e5f0e0b35316eaec7e221c295d0ed437a21
martin059 <41234752+martin059@users.noreply.github.com>
2024-03-31 19:22:37 +0200

    Add rudimentary About page

--- svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/+page.svelte
+++ svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+// @ts-nocheck
+</script>
+
 <h1>Hello World to the Front end app!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation.............</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Find out more in the <a href="/about">About</a> section!</p>
---
+++ svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/about/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let name = 'Svelte Front End App';
+</script>
+
+<style>
+    main {
+        padding: 2rem;
+        text-align: center;
+    }
+
+    h1 {
+        font-size: 2rem;
+        margin-bottom: 1rem;
+    }
+
+    p, ul {
+        font-size: 1.2rem;
+    }
+
+    ul {
+        text-align: center;
+        list-style-position: inside;
+    }
+</style>
+
+<main>
+    <h1>About { name }</h1>
+    <p>Welcome to the about page of { name }! This application is meant to serve as the Front End for the main App which consist of a ToDo app.</p>
+    <p>The main app is composed of three docker containers: </p>
+    <ul>
+        <li><strong>Postgres Container</strong>: Contains the SQL database which stores all information for the app</li>
+        <li><strong>Python Container</strong>: Will serve as the Back End for the app usign Flask and Python</li>
+        <li><strong>Svelte Container</strong>: Will serve as the Front End for the app using Svelte and TypeScript</li>
+    </ul>
+</main>
