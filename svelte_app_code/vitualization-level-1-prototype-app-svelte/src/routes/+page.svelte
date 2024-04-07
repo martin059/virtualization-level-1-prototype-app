848c8ff8f4c57d9fc550d9ea1aa1284a47a6996d
martin059 <41234752+martin059@users.noreply.github.com>
2024-04-07 15:46:07 +0200

    Add import sveltestrap, and replaced tabs

--- svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/+page.svelte
+++ svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 // @ts-nocheck
+  import { Button, Col, Row } from '@sveltestrap/sveltestrap';
 </script>
 
 <h1>Hello World to the Front end app!</h1>
@@ -8,3 +9,10 @@
 
 <h2>View tasks</h2>
 <p><a href="/tasks">Go to tasks view</a></p>
+
+<h2>Sveltestrap Test</h2>
+<Row>
+  <Col>
+    <Button color="primary" outline>Hello Sveltestrap!</Button>
+  </Col>
+</Row>
