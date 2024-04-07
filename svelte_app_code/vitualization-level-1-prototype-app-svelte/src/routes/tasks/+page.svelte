9f5b43c21019ad16cc6002d38eee342f5a3aa4ca
martin059 <41234752+martin059@users.noreply.github.com>
2024-04-07 20:10:40 +0200

    Added loading spinner prototype

--- svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/tasks/+page.svelte
+++ svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/tasks/+page.svelte
@@ -8,6 +8,7 @@
 
     let response: any;
     let tasks: Task[] = [];
+    let isLoading = true;
 
     onMount(async () => {
         try {
@@ -16,36 +17,42 @@
             tasks = response as Task[];
         } catch (error) {
             console.error(error);
+        } finally {
+            isLoading = false;
         }
     });
 </script>
 <NavBar />
 
 <h1>Task List</h1>
-<div>
-    <table>
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Creation Date</th>
-                <th>Status</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each tasks as task (task.id)}
+{#if isLoading}
+    <div class="loading-spinner"></div>
+{:else}
+    <div>
+        <table>
+            <thead>
                 <tr>
-                    <td>{task.id}</td>
-                    <td>{task.task_name}</td>
-                    <td>{task.task_descrip}</td>
-                    <td>{new Date(task.creation_date).toLocaleDateString('en-GB', { year: 'numeric', month: 'long', day: '2-digit' })}</td>
-                    <td>{task.task_status}</td>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Creation Date</th>
+                    <th>Status</th>
                 </tr>
-            {/each}
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                {#each tasks as task (task.id)}
+                    <tr>
+                        <td>{task.id}</td>
+                        <td>{task.task_name}</td>
+                        <td>{task.task_descrip}</td>
+                        <td>{new Date(task.creation_date).toLocaleDateString('en-GB', { year: 'numeric', month: 'long', day: '2-digit' })}</td>
+                        <td>{task.task_status}</td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    </div>
+{/if}
 
 <style>
     div {
@@ -61,5 +68,20 @@
     th, td {
         border: 1px solid black;
         padding: 8px;
+    }
+
+    .loading-spinner {
+        border: 16px solid #f3f3f3;
+        border-top: 16px solid #3498db;
+        border-radius: 50%;
+        width: 120px;
+        height: 120px;
+        animation: spin 2s linear infinite;
+        margin: auto;
+    }
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
     }
 </style>
