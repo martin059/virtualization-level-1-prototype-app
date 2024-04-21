b36b0a91889b2d69b38c7c89a257d944494a7bfc
martin059 <41234752+martin059@users.noreply.github.com>
2024-04-21 20:33:01 +0200

    First form draft, needs to be polished

--- svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/tasks/new/+page.svelte
+++ svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/tasks/new/+page.svelte
@@ -9,5 +9,27 @@
   </Col>
   <Col class="col-11 d-flex align-items-center flex-column">
     <h1>New Task</h1>
+      <Col class="col-6">
+        <form>
+          <div class="mb-3">
+            <label for="taskName" class="form-label">Task Name</label>
+            <input type="text" class="form-control" id="taskName" />
+          </div>
+          <div class="mb-3">
+            <label for="taskDescription" class="form-label">Task Description</label>
+            <textarea class="form-control" id="taskDescription" rows="3"></textarea>
+          </div>
+          <div class="mb-3">
+            <label for="taskStatus" class="form-label">Task Status</label>
+            <select class="form-select" id="taskStatus">
+              <option selected>Choose...</option>
+              <option value="1">To Do</option>
+              <option value="2">In Progress</option>
+              <option value="3">Done</option>
+            </select>
+          </div>
+          <Button type="submit" class="btn btn-primary">Submit</Button>
+        </form>
+      </Col>
   </Col>
 </main>
