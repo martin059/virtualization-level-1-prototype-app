bb5c7f2dddbc364c901872c95f08d1a2164a4dab
martin059 <41234752+martin059@users.noreply.github.com>
2024-05-05 01:26:48 +0200

    Added view for task details

--- svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/tasks/[taskId]/+page.svelte
+++ svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/tasks/[taskId]/+page.svelte
@@ -1,16 +1,54 @@
 <script lang="ts">
     import { page } from '$app/stores';
-    import NavBar from "@components/navBar.svelte";
+    import { onMount } from 'svelte';
+    import { Col, NavBar, Button } from "@components/commonComponents";
+    import LoadingSpinner from '@components/loadingSpinner.svelte';
+    import type { Task } from "@models/Task";
+    import { Notifications, acts } from '@tadashi/svelte-notification'
+    import { goto } from '$app/navigation';
 
     let taskId: string | null = $page.params.taskId;
+    let response: any;
+    let task: Task;
+    let isLoading: boolean = true;
+
+    onMount(async () => {
+        try {
+            const res = await fetch('http://localhost:5001/tasks/' + taskId);
+            const statusCode = res.status;
+            if (statusCode >= 200 && statusCode < 300) {
+                response = await res.json();
+                task = response[0] as Task;
+            } else {
+                acts.add({ mode: 'error', message: 'Something went wrong, for more info consult the console.', lifetime: 3});
+                console.log(response);
+            }
+        } catch (error) {
+            acts.add({ mode: 'error', message: 'Something went wrong, for more info consult the console.', lifetime: 3 });
+            console.error(error);
+        } finally {
+            isLoading = false;
+        }
+    });
 </script>
-<NavBar />
-<main>
-    <h1>Task Details</h1>
-    <p>Task ID: {taskId}</p>
-    <!-- Add more details about the task here -->
+
+<main class="d-flex flex-row full-height">
+    <Col class="col-1">
+        <NavBar />
+    </Col>
+    <Col class="col-11 d-flex align-items-center flex-column">
+        <h1>Task Details</h1>
+        <Notifications />
+        {#if isLoading}
+            <LoadingSpinner />
+        {:else}
+            <p>Task ID: {task.id}</p>
+            <p>Task Name: {task.task_name}</p>
+            <p>Task Description: {task.task_descrip}</p>
+            <p>Task Status: {task.task_status}</p>
+        {/if}
+    </Col>
 </main>
 
 <style>
-    /* Add your styles here */
 </style>
