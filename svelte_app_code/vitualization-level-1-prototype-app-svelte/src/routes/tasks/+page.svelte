661919836bb0de9bbbd380b5c323970bedc99a61
L. Martin <41234752+martin059@users.noreply.github.com>
2024-05-09 00:54:33 +0200

    #19 add delete done buttons (#51)
    * Add initial button structure
    
    * Refactored actions to improve design
    
    * Minor modification of function
    
    * Minor UX improvement

--- svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/tasks/+page.svelte
+++ svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/tasks/+page.svelte
@@ -10,8 +10,9 @@
     let response: any;
     let tasks: Task[] = [];
     let isLoading: boolean = true;
+    let updateInProgress: boolean = false;
 
-    onMount(async () => {
+    async function fetchData() {
         try {
             const res = await fetch('http://localhost:5001/tasks');
             const statusCode = res.status;
@@ -29,11 +30,51 @@
         } finally {
             isLoading = false;
         }
-    });
+    }
+
+    onMount(fetchData);
 
     function gotToNewTaskForm() {
         goto('/tasks/new');
     }
+
+    async function updateTaskStatus(taskId: number, newStatus: string) {
+        if (updateInProgress) {
+          acts.add({mode: "warn",message: "Update in progress. Wait until a response is returned.", lifetime: 3});
+          return;
+        }
+        updateInProgress = true;
+        try {
+          let jsonString: string = `{"task_status":"${newStatus}"}`;
+          const res = await fetch("http://localhost:5001/tasks/" + taskId, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: jsonString,
+            timeout: 10000, // 10 seconds
+          });
+          const statusCode = await res.status;
+          if (statusCode >= 200 && statusCode < 300) {
+            acts.add({ mode: "success", message: "Task " + taskId + " updated", lifetime: 3 });
+            fetchData();
+          } else {
+            const response = await res.json();
+            acts.add({
+              mode: "error",
+              message: "Something went wrong, for more info consult the console.",
+            });
+            console.log("Status response code: " + statusCode + ";Response: ");
+            console.log(response);
+          }
+        } catch (error) {
+          acts.add({mode: "error",message: "Something went wrong, for more info consult the console.",});
+          console.error(error);
+        } finally {
+          updateInProgress = false;
+        }
+    }
+
 </script>
 
 <main class="d-flex flex-row full-height">
@@ -57,6 +98,7 @@
                                 <th>Creation Date</th>
                                 <th>Status</th>
                                 <th>Details</th>
+                                <th>Actions</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -68,6 +110,16 @@
                                     <td>{new Date(task.creation_date).toLocaleDateString('en-GB', { year: 'numeric', month: 'long', day: '2-digit' })}</td>
                                     <td>{task.task_status}</td>
                                     <td><Button color="info" on:click={() => goto(`/tasks/${task.id}`)}>View</Button></td>
+                                    {#if task.task_status === 'Done' || task.task_status === 'Dropped'}
+                                        <td><Button color="danger" on:click={() => updateTaskStatus(task.id, 'Deleted')}>Delete</Button></td>
+                                    {:else if task.task_status !== 'Deleted'}
+                                        <td>
+                                            <Button color="success" on:click={() => updateTaskStatus(task.id, 'Done')}>Done</Button>
+                                            <Button color="warning" on:click={() => updateTaskStatus(task.id, 'Dropped')}>Drop</Button>
+                                        </td>
+                                    {:else}
+                                    <td><Button color="secondary" disabled>N/A</Button></td>
+                                    {/if}
                                 </tr>
                             {/each}
                         </tbody>
