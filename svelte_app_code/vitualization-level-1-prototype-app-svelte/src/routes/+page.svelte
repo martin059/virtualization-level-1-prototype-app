4b33a6708bb5647593029a06d4937134b58c3b4e
martin059 <41234752+martin059@users.noreply.github.com>
2024-05-11 20:50:19 +0200

    Added sample todo logo to app

--- svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/+page.svelte
+++ svelte_app_code/vitualization-level-1-prototype-app-svelte/src/routes/+page.svelte
@@ -8,9 +8,10 @@
     <NavBar />
   </Col>
   <Col class="col-11 d-flex align-items-center flex-column">
-    <h1>Hello World to the Front end app!</h1>
+    <h1><img src="/ToDo-icon.svg" alt="test" id="logo"> Svelte Front End ToDo App</h1>
     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
     <p>Find out more in the <a href="/about">About</a> section!</p>
+    
 
     <h2>View tasks</h2>
     <p><a href="/tasks">Go to tasks view</a></p>
@@ -23,3 +24,10 @@
     </Row>
   </Col>
 </main>
+
+<style>
+  #logo {
+    width: 1.75em;
+    height: 1.75em;
+  }
+</style>
