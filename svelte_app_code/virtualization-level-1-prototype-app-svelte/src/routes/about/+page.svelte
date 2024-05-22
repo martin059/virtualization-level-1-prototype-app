b2cfd5d4473faff8731894b8c828cabfd577d5f4
martin059 <41234752+martin059@users.noreply.github.com>
2024-05-22 22:23:28 +0200

    Updated about comment

--- svelte_app_code/virtualization-level-1-prototype-app-svelte/src/routes/about/+page.svelte
+++ svelte_app_code/virtualization-level-1-prototype-app-svelte/src/routes/about/+page.svelte
@@ -36,7 +36,7 @@
                     <strong>Svelte Container</strong>: It serves as the Front End for the app using Svelte, <a href="https://www.typescriptlang.org/"><img src="/Typescript.svg" alt="TypeScript logo" class="about-logo text-logo"></a> TypeScript and Sveltestrap.
                 </li>
             </ul>
-            <p>This whole containerized web app was entirely developed in a Linux Virtual Machine using a <a href="https://manjaro.org/"><img src="/manjaro-logo.svg" alt="Manjaro logo" class="about-logo text-logo"></a> Manjaro distro setup with <a href="https://www.vagrantup.com/"><img src="/vagrant.svg" alt="Vagrant logo" class="about-logo text-logo"></a> Vagrant and configured using an <a href="https://www.ansible.com/"><img src="/ansible.svg" alt="Ansible logo" class="about-logo text-logo"></a> Ansible script.</p>
+            <p>This entire containerized web app was developed in a Linux virtual machine using the <a href="https://manjaro.org/"><img src="/manjaro-logo.svg" alt="Manjaro logo" class="about-logo text-logo"></a> Manjaro distro. It was later compiled and tested in an <a href="https://almalinux.org/"><img src="/AlmaLinux_Icon_Logo.svg" alt="AlmaLinux logo" class="about-logo text-logo"></a> AlmaLinux 9 setup with <a href="https://www.vagrantup.com/"><img src="/vagrant.svg" alt="Vagrant logo" class="about-logo text-logo"></a> Vagrant and configured using an <a href="https://www.ansible.com/"><img src="/ansible.svg" alt="Ansible logo" class="about-logo text-logo"></a> Ansible script.</p>
         </div>
     </Col>
 </main>
